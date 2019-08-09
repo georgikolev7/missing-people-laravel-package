4efd7de6d218de3706aec89ea1dfb8e20735dfa2
Georgi Kolev <suncho_26@aol.com>
2019-08-09 12:59:38 +0200

    Add RegionController

---
+++ src/Http/Controllers/RegionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Slavic\MissingPersons\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class RegionController extends Controller
+{
+    
+    public function index()
+    {
+        
+    }
+}
