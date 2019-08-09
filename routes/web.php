63caba76b45e4dad2e4eacc8ffdfe71b79ed6b77
Georgi Kolev <suncho_26@aol.com>
2019-08-09 13:00:31 +0200

    Update routes for regions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
-<?php
-    
-    Route::get('missing', function() {
-        return 'missing';
+<?php
+    
+    // Clear cache
+    Route::get('/clear-cache', function() {
+        Artisan::call('cache:clear');
+        return "Cache is cleared";
     });
+    
+    // Regions
+    Route::get('regions', 'RegionController@index')->name('regions.index');
+    Route::get('regions/create', 'RegionController@create')->name('regions.create');
+    Route::get('regions/delete', 'RegionController@delete')->name('regions.delete');
+    Route::resource('regions', 'RegionController');
