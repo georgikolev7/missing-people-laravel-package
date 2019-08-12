a00decd7c112e2068a0f8ea0dfd1ff68ddf07072
Georgi Kolev <suncho_26@aol.com>
2019-08-12 08:23:39 +0200

    Add routes for "Persons"

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,21 @@
-<?php
+<?php
+    
+    // Clear cache
+    Route::get('/clear-cache', function() {
+        Artisan::call('cache:clear');
+        return "Cache is cleared";
+    });
     
-    // Clear cache
-    Route::get('/clear-cache', function() {
-        Artisan::call('cache:clear');
-        return "Cache is cleared";
-    });
-    
-    // Regions
-    Route::get('regions', 'RegionController@index')->name('regions.index');
-    Route::get('regions/create', 'RegionController@create')->name('regions.create');
-    Route::get('regions/delete', 'RegionController@delete')->name('regions.delete');
-    Route::resource('regions', 'RegionController');
+    // Regions
+    Route::get('regions', 'RegionController@index')->name('regions.index');
+    Route::get('regions/create', 'RegionController@create')->name('regions.create');
+    Route::get('regions/delete', 'RegionController@delete')->name('regions.delete');
+    Route::resource('regions', 'RegionController');
+    
+    // Persons
+    Route::get('persons', 'PersonController@index')->name('persons.index');
+    Route::get('persons/edit/{hash}', 'PersonController@edit')->name('persons.edit');
+    Route::get('persons/view/{hash}', 'PersonController@view')->name('persons.view');
+    Route::get('persons/create', 'PersonController@create')->name('persons.create');
+    Route::get('persons/delete', 'PersonController@delete')->name('persons.delete');
+    Route::resource('persons', 'PersonController');
