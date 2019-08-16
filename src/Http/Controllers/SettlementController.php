bf7782651872cfeec7c6561909b48734698f6cb1
Georgi Kolev <suncho_26@aol.com>
2019-08-16 12:33:49 +0200

    Създаване на SetllementController

---
+++ src/Http/Controllers/SettlementController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Slavic\MissingPersons\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SettlementController extends Controller
+{
+    
+    public function fetch(Request $request)
+    {
+        $settlements = \Slavic\MissingPersons\Model\Settlement::getByRegion($request->region_id);
+        return \Response::json(['settlements' => $settlements], 200);
+    }
+}
