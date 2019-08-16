04e51068951fed8f443fbcbc8d7de69aee54fbe2
Georgi Kolev <suncho_26@aol.com>
2019-08-16 12:30:50 +0200

    Update routes.php

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,31 @@
-<?php
-    
-    // Clear cache
-    Route::get('/clear-cache', function() {
-        Artisan::call('cache:clear');
-        return "Cache is cleared";
-    });
+<?php
     
-    // Regions
-    Route::get('regions', 'RegionController@index')->name('regions.index');
-    Route::get('regions/create', 'RegionController@create')->name('regions.create');
-    Route::get('regions/delete', 'RegionController@delete')->name('regions.delete');
-    Route::resource('regions', 'RegionController');
-    
-    // Persons
-    Route::get('persons', 'PersonController@index')->name('persons.index');
-    Route::get('persons/edit/{hash}', 'PersonController@edit')->name('persons.edit');
-    Route::get('persons/view/{hash}', 'PersonController@view')->name('persons.view');
-    Route::get('persons/create', 'PersonController@create')->name('persons.create');
-    Route::get('persons/delete', 'PersonController@delete')->name('persons.delete');
+    // Clear cache
+    Route::get('/clear-cache', function() {
+        Artisan::call('cache:clear');
+        return "Cache is cleared";
+    });
+    
+    // Index page
+    Route::get('/', 'PersonController@index')->name('persons.index');
+    
+    // Regions
+    Route::get('regions', 'RegionController@index')->name('regions.index');
+    Route::get('regions/create', 'RegionController@create')->name('regions.create');
+    Route::get('regions/delete', 'RegionController@delete')->name('regions.delete');
+    Route::resource('regions', 'RegionController');
+    
+    // Settlements
+    Route::get('settlements/fetch/{region_id}', 'SettlementController@fetch')->name('settlements.fetch');
+    
+    // Persons
+    Route::get('persons', 'PersonController@index')->name('persons.index');
+    Route::get('persons/edit/{hash}', 'PersonController@edit')->name('persons.edit');
+    Route::get('persons/view/{hash}', 'PersonController@view')->name('persons.view');
+    Route::post('persons/store', 'PersonController@store')->name('persons.store');
+    Route::get('persons/create', 'PersonController@create')->name('persons.create');
+    Route::get('persons/delete', 'PersonController@delete')->name('persons.delete');
+    Route::post('persons/photo/store', 'PersonController@store_photo')->name('persons.store_photo');
+    Route::get('persons/photo/list', 'PersonController@list_photo')->name('persons.list_photo');
+    Route::post('persons/photo/list/sort_order', 'PersonController@list_photo_sort')->name('persons.list_photo_sort');
     Route::resource('persons', 'PersonController');
