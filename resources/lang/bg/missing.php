59a5de9042ff501643895eb8763b0259d0442f1c
Georgi Kolev <suncho_26@aol.com>
2019-08-19 12:46:54 +0200

    Update translation phrases

--- resources/lang/bg/missing.php
+++ resources/lang/bg/missing.php
@@ -6,8 +6,18 @@ return [
     'region_name' => 'Име на областта',
     'ekatte' => 'ЕКАТТЕ',
     'please_fill_region_name' => 'Моля, попълнете правилно името на областта',
+    'please_fill_person_full_name' => 'Моля, попълнете трите имена на лицето',
     'placeholder_region_name' => 'пример: Област Видин',
     'description' => 'Описание',
+    
+    'missing_person' => 'Изчезнало лице',
+    'wanted_person' => 'Издирвано лице',
+    
+    'year_short' => 'г.',
+    'cm' => 'см.',
+    
+    'edit' => 'Промяна',
+    'delete' => 'Изтриване',
     
     'person_name' => 'Име на лицето',
     'age' => 'Възраст',
