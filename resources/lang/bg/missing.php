81e61c74b0c7a10e93b530af447ec0c3fbd9ac1a
Georgi Kolev <suncho_26@aol.com>
2019-08-22 13:22:33 +0200

    Обновяване на езиците

--- resources/lang/bg/missing.php
+++ resources/lang/bg/missing.php
@@ -9,28 +9,23 @@ return [
     'please_fill_person_full_name' => 'Моля, попълнете трите имена на лицето',
     'placeholder_region_name' => 'пример: Област Видин',
     'description' => 'Описание',
-    
+    'submit_person' => 'Публикуване на издирваното лице',
     'missing_person' => 'Изчезнало лице',
     'wanted_person' => 'Издирвано лице',
-    
     'year_short' => 'г.',
     'cm' => 'см.',
-    
     'edit' => 'Промяна',
     'delete' => 'Изтриване',
-    
     'person_name' => 'Име на лицето',
     'age' => 'Възраст',
     'gender' => 'Пол',
     'height' => 'Ръст',
     'year_of_birth' => 'Година на раждане',
     'last_seen_on' => 'Последно забелязан/а',
-    
     'map' => 'Карта',
     'missing_persons' => 'Изчезнали лица',
     'wanted_persons' => 'Издирвани лица',
     'add_person' => 'Обявяване',
-    
     'site_title' => 'Издирвани лица на територията на България',
     'default_meta_title' => 'Издирвани лица на територията на България',
     'default_meta_keywords' => 'издирвани лица, безследно изчезнали, осъдени лица',
