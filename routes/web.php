a13cddef1c1f0dee19af33750a91b73958dbc766
Georgi Kolev <suncho_26@aol.com>
2019-08-23 15:08:56 +0200

    Reorder persons routes

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,21 @@
     
     // Persons
     Route::get('persons', 'PersonController@index')->name('persons.index');
-    Route::get('persons/edit/{hash}', 'PersonController@edit')->name('persons.edit')->middleware('auth');
     Route::get('persons/view/{hash}', 'PersonController@view')->name('persons.view');
-    Route::post('persons/store', 'PersonController@store')->name('persons.store')->middleware('auth');
-    Route::post('persons/update/{hash}', 'PersonController@update')->name('persons.update')->middleware('auth');
     Route::get('persons/create', 'PersonController@create')->name('persons.create');
-    Route::get('persons/delete/{hash}', 'PersonController@delete')->name('persons.delete')->middleware('auth');
-    Route::post('persons/photo/store', 'PersonController@store_photo')->name('persons.store_photo')->middleware('auth');
-    Route::get('persons/photo/list', 'PersonController@list_photo')->name('persons.list_photo')->middleware('auth');
-    Route::post('persons/photo/list/sort_order', 'PersonController@list_photo_sort')->name('persons.list_photo_sort')->middleware('auth');
+    
+    // Persons restricted pages
+    Route::middleware(['auth'])->group(function () {
+        Route::get('persons/edit/{hash}', 'PersonController@edit')->name('persons.edit');
+        Route::get('persons/delete/{hash}', 'PersonController@delete')->name('persons.delete');
+        Route::get('persons/photo/list', 'PersonController@list_photo')->name('persons.list_photo');
+        
+        Route::post('persons/store', 'PersonController@store')->name('persons.store');
+        Route::post('persons/update/{hash}', 'PersonController@update')->name('persons.update');
+        Route::post('persons/photo/store', 'PersonController@store_photo')->name('persons.store_photo');
+        Route::post('persons/photo/list/sort_order', 'PersonController@list_photo_sort')->name('persons.list_photo_sort');
+    });
+    
     Route::resource('persons', 'PersonController');
     
     // Sitemap
