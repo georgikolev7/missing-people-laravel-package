3230dc50c7e30f204bca386719feea53cb0aa082
Georgi Kolev <suncho_26@aol.com>
2019-09-04 15:13:02 +0200

    Format JS

--- public/js/pages/map.page.js
+++ public/js/pages/map.page.js
@@ -1,36 +1,35 @@
 $(function() {
-    
+
     var markers = [];
-    
+
     var map = L.map('map-box', {
         center: [42.7339, 25.4858],
         minZoom: 2,
         zoom: 7
     });
-    
+
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: ['a', 'b', 'c']
     }).addTo(map);
-    
+
     var markerClusters = L.markerClusterGroup();
-    
+
     $.each(window.locations, function(idx, person) {
-        
         var myIcon = L.icon({
             iconUrl: 'storage/' + person.icon,
             iconRetinaUrl: 'vendor/missing/js/leafletjs/images/marker-icon-x2.png',
-            iconSize: [35, 45],
+            iconSize: [45, 45],
             iconAnchor: [9, 21],
             popupAnchor: [0, -14]
         });
-        
+
         var m = L.marker([person.lat, person.lng], {
                 icon: myIcon
             })
             .bindPopup('<a href="' + person.url + '" target="_blank">' + person.name + '</a>');
         markerClusters.addLayer(m);
     });
-    
+
     map.addLayer(markerClusters);
 });
--- public/js/pages/persons.view.js
+++ public/js/pages/persons.view.js
@@ -1,13 +1,12 @@
 $(function() {
-    
+
     $('button#set-as-found').on('click', function(e) {
-       e.preventDefault();
+        e.preventDefault();
 
-       var data = { '_token': $('meta[name="csrf-token"]').attr('content') };
+        var data = {
+            '_token': $('meta[name="csrf-token"]').attr('content')
+        };
 
-       $.post($(this).data('href'), data, function(response) {
-           
-       });
-   });
-    
+        $.post($(this).data('href'), data, function(response) {});
+    });
 });
