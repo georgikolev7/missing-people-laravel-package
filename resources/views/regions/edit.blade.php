9bf2c5456625160f5d4c0dc8f0191b302a10547f
Georgi Kolev <suncho_26@aol.com>
2019-09-06 10:27:21 +0200

    Clear empty spaces

--- resources/views/regions/edit.blade.php
+++ resources/views/regions/edit.blade.php
@@ -14,14 +14,14 @@
                 </label>
                 <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3" name="name" id="region-name" type="text" placeholder="@lang('missing.placeholder_region_name')">
                 <p class="text-red text-xs italic">@lang('missing.please_fill_region_name')</p>
-            </div>
+            </div>
             <div class="md:w-1/2 px-3">
                 <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name">
                     @lang('missing.ekatte')
                 </label>
                 <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" id="grid-last-name" type="text" placeholder="Doe">
             </div>
-      </div>
+		</div>
     </div>
 @endsection
 
