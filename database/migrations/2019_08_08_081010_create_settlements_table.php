9045d454abc0a8d7554ac21e0f067883898409da
Georgi Kolev <suncho_26@aol.com>
2019-09-12 08:52:14 +0200

    Обновяване на колона "EKATTE"

--- database/migrations/2019_08_08_081010_create_settlements_table.php
+++ database/migrations/2019_08_08_081010_create_settlements_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use DB;
 
 class CreateSettlementsTable extends Migration
 {
@@ -19,7 +20,10 @@ class CreateSettlementsTable extends Migration
             $table->decimal('lat', 10, 8);
             $table->decimal('lng', 11, 8);
             $table->integer('people')->unsigned();
+            $table->timestamps();
         });
+        
+        DB::statement('ALTER TABLE settlements CHANGE ekatte ekatte INT(5) UNSIGNED ZEROFILL NOT NULL');
     }
     
     public function down()
